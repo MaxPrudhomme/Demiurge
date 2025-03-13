41c3a7ac8628132ddce0a268fe191c5f2e071f63
Max Prudhomme <account@maxprudhomme.com>
2025-03-13 15:21:26 +0100

    Initial Commit

---
+++ Demiurge/DemiurgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemiurgeApp.swift
+//  Demiurge
+//
+//  Created by Max PRUDHOMME on 13/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DemiurgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
